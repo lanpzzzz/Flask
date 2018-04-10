5d7aa3196c38fe6691db29a6964e095c11d95e16
lanpzzzz <532891679@qq.com>
2018-04-10 08:25:36 -0400

    change host

--- hello.py
+++ hello.py
@@ -1,14 +1,16 @@
 from flask import Flask
+from flask.ext.script import Manager
 from flask import redirect
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
 	return redirect('http://www.baidu.com')
 
 if __name__=='__main__':
-    app.run(debug=True)
+    manager.run()
     
 
         
