2a893621993fd6f0c6798c3027f2227876ecacc4
lanpzzzz <532891679@qq.com>
2018-04-09 22:39:27 -0400

    try make_response()

--- hello.py
+++ hello.py
@@ -1,12 +1,13 @@
 from flask import Flask
-from flask import request
+from flask import make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	user_agent = request.headers.get('User-Agent')
-	return '<h1>Your browers is %s</h1>' % user_agent
+	response = make_response('<h1>This document carries a cookie!</h1>')
+	response.set_cookie('answer','42')
+	return response
 
 if __name__=='__main__':
     app.run(debug=True)
