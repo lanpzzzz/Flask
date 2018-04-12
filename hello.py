136c8e6f3145866a0de34f9c6d8767006057e1b4
lanpzzzz <532891679@qq.com>
2018-04-12 05:25:20 -0400

    try render conditional control structure

--- hello.py
+++ hello.py
@@ -1,16 +1,17 @@
-from flask import Flask
-from flask.ext.script import Manager
-from flask import redirect
+from flask import Flask,render_template
 
 app = Flask(__name__)
-manager = Manager(app)
 
 @app.route('/')
 def index():
-	return redirect('http://www.baidu.com')
+	return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html',name=name)
 
 if __name__=='__main__':
-    manager.run()
+    app.run(debug = True)
     
 
         
