15c73ccdd09eff6a4e0948f1399e05ed326f3b7f
lanpzzzz <532891679@qq.com>
2018-04-13 03:30:22 -0400

    自定义错误界面

--- hello.py
+++ hello.py
@@ -1,17 +1,20 @@
 from flask import Flask,render_template
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
-def index():
-	return render_template('index.html')
-
-@app.route('/user/')
 def user():
-	return render_template('user.html',comments=(1,2,1,2))
+	return render_template('user.html',comments=(1,2,3,4,1,2,3))
+
+@app.route('/user/<name>')
+def index(name):
+	return render_template('index.html',name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
 
 if __name__=='__main__':
-    app.run(debug = True)
-    
-
-        
+	app.run()
