ee22974a17396e9bb044ae888216ba088ebfbaf8
lanpzzzz <532891679@qq.com>
2018-04-14 05:08:07 -0400

    learn 表单

--- hello.py
+++ hello.py
@@ -2,14 +2,29 @@ from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+
+class NameForm(FlaskForm):
+	name=StringField('What is you name?',validators=[DataRequired()])
+	submit=SubmitField('Submit')
+
+@app.route('/',methods=['GET','POST'])
 def user():
-	return render_template('user.html',current_time=datetime.utcnow())
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('user.html',form=form,name=name,current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def index(name):
